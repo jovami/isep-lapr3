7ca7b5b6636ff75d42a82515ab8fe83a2d198e06
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-11-07 09:57:08 +0000

    fix: Update test class

---
+++ src/test/java/jovami/MainTest.java
@@ -0,0 +1,19 @@
+package jovami;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
