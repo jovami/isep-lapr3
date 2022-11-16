af89761ad7400a0df09464e0762cd468202a9dcc
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2022-11-16 19:26:27 +0000

    feat(graphs): implementation of BreadthFirstSearch algorithm

---
+++ src/main/java/jovami/graph/Algorithms.java
@@ -0,0 +1,39 @@
+package jovami.graph;
+
+import java.util.*;
+
+public class Algorithms {
+
+    /**
+     * Performs breadth-first search of a Graph starting in a vertex
+     *
+     * @param g    Graph instance
+     * @param vert vertex that will be the source of the search
+     * @return a LinkedList with the vertices of breadth-first search
+     */
+    public static <V, E> LinkedList<V> BreadthFirstSearch(Graph<V, E> g, V vert) {
+        if (!g.vertices().contains(vert))
+            return null;
+
+        LinkedList<V> qbfs = new LinkedList<>();
+        LinkedList<V> qaux = new LinkedList<>();
+        boolean[] visited = new boolean[g.numVertices()];
+
+        qbfs.add(vert);
+        qaux.add(vert);
+        visited[g.key(vert)] = true;
+
+        while (!qaux.isEmpty()) {
+            vert = qaux.getFirst();
+            qaux.removeFirst();
+            for (V vAdj : g.adjVertices(vert)) {
+                if (!visited[g.key(vAdj)]) {
+                    qbfs.add(vAdj);
+                    qaux.add(vAdj);
+                    visited[g.key(vAdj)] = true;
+                }
+            }
+        }
+        return qbfs;
+    }
+}
