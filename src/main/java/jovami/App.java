a2053849f4835c5eea17d1ae7052ea6b4a2310b5
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-11-24 00:19:00 +0000

    feat(esinf/classes): add Basic UserStore class

--- src/main/java/jovami/App.java
+++ src/main/java/jovami/App.java
@@ -1,13 +1,21 @@
 package jovami;
 
+import jovami.model.store.UserStore;
+
 /**
  * App
  * Singleton-pattern based class
  */
 public class App {
 
+    private UserStore userStore;
+
     private App() {
-        // Nothing for now
+        this.userStore = new UserStore();
+    }
+
+    public UserStore userStore() {
+        return this.userStore;
     }
 
     /* singleton pattern */
---
+++ src/main/java/jovami/model/store/UserStore.java
@@ -0,0 +1,18 @@
+package jovami.model.store;
+
+import java.util.Iterator;
+
+import jovami.model.User;
+
+/**
+ * UserStore
+ */
+public class UserStore implements Iterable<User> {
+
+    // TODO: Impl class with graphs
+
+    @Override
+    public Iterator<User> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
