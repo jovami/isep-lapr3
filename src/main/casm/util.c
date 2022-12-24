5a67978ec62f2bf2fb71dc8ff07d2960e22ce414
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-12-24 19:35:56 +0000

    feat(arqcp): add alloc utils

--- src/main/casm/util.c
+++ src/main/casm/util.c
@@ -1,9 +1,21 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
 
 #include <util.h>
+
+__attribute__((__always_inline__))
+static inline int __check_overflow(size_t n, size_t m);
+
+inline int
+__check_overflow(size_t m, size_t n)
+{
+    /* NOTE: taken from musl libc
+     * ⬝https://github.com/bminor/musl/blob/master/src/malloc/calloc.c
+     */
+    return n && m > (size_t) -1/n;
+}
 
 
 void
@@ -22,3 +34,28 @@ die(const char *restrict fmt, ...)
 
     exit(EXIT_FAILURE);
 }
+
+void *
+arqcp_calloc(size_t nmemb, size_t size)
+{
+    void *p;
+
+    if (!(p = calloc(nmemb, size)))
+        die("arqcp_calloc: ");
+    return p;
+}
+
+void *
+arqcp_malloc(size_t nmemb, size_t size)
+{
+    void *p;
+
+    if (__check_overflow(nmemb, size))
+        die("arqcp_malloc: overflow detected with params %zu and %zu",
+            nmemb, size);
+    else if (!(p = malloc(nmemb * size)))
+        die("arqcp_malloc: ");
+
+    return p;
+}
+
