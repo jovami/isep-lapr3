6f7fd8ae20e00ef46044efded4eb416692f91f80
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-12-25 18:45:31 +0000

    feat(arqcp): rework tests 'framework'

--- src/test/casm/tests.h
+++ src/test/casm/tests.h
@@ -1,5 +1,28 @@
 #pragma once
 
+#include <stddef.h>
+
+typedef struct test_case test_case;
+
+struct test_case {
+    const char *restrict name;
+    void (*test)(void);
+};
 
 /* US101 */
 void rng_gen_run(void);
+
+/* US110 */
+void sens_vec_run(void);
+
+
+
+
+/* test cases */
+const test_case tests[] = {
+    { .name = "US101", .test = &rng_gen_run     },
+    { .name = "US110", .test = &sens_vec_run    },
+
+    /* sentinel */
+    { NULL, NULL }
+};
