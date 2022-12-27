d35efdc41753a42e5dc6e95066e2145f2f5ff168
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-12-27 22:41:08 +0000

    fix(arqcp/dailymatrix): update dailymatrix for the new Sensor struct

--- src/main/casm/dailymatrix/dailymatrix.h
+++ src/main/casm/dailymatrix/dailymatrix.h
@@ -1,11 +1,5 @@
 #pragma once
 
-void
-daily_matrix(
-    const char *data_temp,
-    const unsigned short *data_dir_vento,
-    const unsigned char *data_velc_vento,
-    const unsigned char *data_humd_atm,
-    const unsigned char *data_humd_solo,
-    const unsigned char *data_pluvio
-);
+#include <sensor_vec.h>
+
+void daily_matrix(sensor_vec *pack);
