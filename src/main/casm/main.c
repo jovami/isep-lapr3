461b0d6a5692a0fd661741e079370dd58c2f7e1d
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-12-28 16:07:09 +0000

    fix(arqcp): remove bootstrap from main and update fn signatures

--- src/main/casm/main.c
+++ src/main/casm/main.c
@@ -7,17 +7,16 @@
 #include <sensor_vec.h>
 #include <util.h>
 
+#include "bootstrap.h"
 #include "menu.h"
 
-static long read_int(char **tmp_buf, size_t *n);
-static void sensor_ammounts(unsigned long sizes[SENS_LAST]);
+static ssize_t read_int(char **tmp_buf, size_t *n);
+static void setnumsensors(size_t sizes[SENS_LAST]);
 
-
-long
+ssize_t
 read_int(char **bufp, size_t *n)
 {
-    long d;
-    ssize_t len;
+    ssize_t d, len;
 
     if ((len = getline(bufp, n, stdin)) == -1)
         die("read_int: failed to read input: ");
@@ -25,7 +24,7 @@ read_int(char **bufp, size_t *n)
     char *line = *bufp;
     line[len-1] = '\0';
 
-    if (sscanf(line, "%ld", &d) <= 0 || d <= 0) {
+    if (sscanf(line, "%zd", &d) <= 0 || d <= 0) {
         errno = EINVAL;
         return 0;
     }
@@ -33,15 +32,14 @@ read_int(char **bufp, size_t *n)
     return d;
 }
 
-
 void
-sensor_ammounts(unsigned long sizes[SENS_LAST])
+setnumsensors(size_t sizes[SENS_LAST])
 {
-    /* NOTE: we use signed long to disallow the
-     * user to get away with typing '-1'
+    /* NOTE: we use ssize_t instead of size_t
+     * to disallow the user to get away with typing '-1'
      * and it being valid due to integer carry
      */
-    signed long amt;
+    ssize_t amt;
 
     char *tmp = NULL;
     size_t n = 0;
@@ -62,13 +60,20 @@ int
 main(int argc, char **argv)
 {
     /* TODO parse argv */
-    unsigned long sizes[SENS_LAST];
-    sensor_ammounts(sizes);
+    unsigned long freqs[SENS_LAST];
+    {
+        /* NOTE: temporary */
+        for (size_t i = 0; i < SENS_LAST; i++)
+            freqs[i] = 1;
+    }
+
+    size_t sizes[SENS_LAST];
+    setnumsensors(sizes);
 
 #if defined (_JOVAMI_DEBUG)
     puts("====DEBUG====");
     for (size_t __i = 0; __i < SENS_LAST; __i++)
-        printf("%s: %lu\n", strsens(__i), sizes[__i]);
+        printf("%s: %zu\n", strsens(__i), sizes[__i]);
     puts("=============\n");
 #endif /* defined (_JOVAMI_DEBUG) */
 
@@ -79,65 +84,11 @@ main(int argc, char **argv)
         vec_init(pack+i, sizes[i]);
     }
 
-    {
-        /* FIXME: do this in a better way */
-        Sensor t1, p1, dv1, vv1, ha1, hs1;
-        sens_init(&t1,
-                  SENS_TEMP,
-                  30,
-                  TEMP_LIM_MAX,
-                  TEMP_LIM_MIN,
-                  2,
-                  MAX_BAD_VALUES);
-        sens_init(&p1,
-                  SENS_PLUV,
-                  70,
-                  PLUVIO_LIM_MAX,
-                  PLUVIO_LIM_MIN,
-                  1,
-                  MAX_BAD_VALUES);
-        sens_init(&dv1,
-                  SENS_DIR_VNT,
-                  70,
-                  DIR_VENTO_LIM_MAX,
-                  DIR_VENTO_LIM_MIN,
-                  2,
-                  MAX_BAD_VALUES);
-        sens_init(&vv1,
-                  SENS_VEL_VNT,
-                  50,
-                  VELC_VENTO_LIM_MAX,
-                  VELC_VENTO_LIM_MIN,
-                  1,
-                  MAX_BAD_VALUES);
-        sens_init(&ha1,
-                  SENS_HUM_ATM,
-                  30,
-                  HUMD_ATM_LIM_MAX,
-                  HUMD_ATM_LIM_MIN,
-                  2,
-                  MAX_BAD_VALUES);
-        sens_init(&hs1,
-                  SENS_HUM_SOL,
-                  40,
-                  HUMD_SOLO_LIM_MAX,
-                  HUMD_SOLO_LIM_MIN,
-                  1,
-                  MAX_BAD_VALUES);
-
-        vec_push(pack+SENS_TEMP, &t1);
-        vec_push(pack+SENS_PLUV, &p1);
-        vec_push(pack+SENS_DIR_VNT, &dv1);
-        vec_push(pack+SENS_VEL_VNT, &vv1);
-        vec_push(pack+SENS_HUM_ATM, &ha1);
-        vec_push(pack+SENS_HUM_SOL, &hs1);
-    }
-
     rnd_init();
+    bootstrap(pack, freqs);
     menu(pack);
 
-    /* daily_matrix(data_temp, data_dir_vento, data_velc_vento, data_humd_atm, data_humd_solo, data_pluvio); */
-
+    /* cleanup */
     for (i = 0; i < SENS_LAST; i++)
         vec_free(pack+i);
     return 0;
