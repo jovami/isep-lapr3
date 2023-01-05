787b4369b49030e30e296614dbfe187638b906a5
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2023-01-05 19:54:27 +0000

    fix(esinf): removed an import

--- src/test/java/jovami/handler/ExpListNProducersHandlerTest.java
+++ src/test/java/jovami/handler/ExpListNProducersHandlerTest.java
@@ -15,7 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class ExpListNProducersHandlerTest {
 
     private ExpListNProducersHandler handler;
-    private NearestHubToClientsHandler nearestHubToClientsHandler;
     private App app;
 
     @BeforeEach
