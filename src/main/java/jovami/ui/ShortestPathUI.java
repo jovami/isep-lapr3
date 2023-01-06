606568134804143fbec089fed2937764b29d22dc
ruben_f_ferreira <1210954@isep.ipp.pt>
2023-01-06 00:58:22 +0000

    feat(esinf/TSP): implement UI for US310

--- src/main/java/jovami/ui/ShortestPathUI.java
+++ src/main/java/jovami/ui/ShortestPathUI.java
@@ -1,6 +1,13 @@
 package jovami.ui;
 
+import java.util.List;
+import java.util.Map;
+
 import jovami.handler.ShortestPathHandler;
+import jovami.model.Distance;
+import jovami.model.User;
+import jovami.model.bundles.Order;
+import jovami.util.io.InputReader;
 
 /**
  * ShortestPathUI
@@ -14,6 +21,42 @@ public class ShortestPathUI implements UserStory {
 
 	@Override
 	public void run() {
-        System.out.println("To be implemented!!");
+        int day = InputReader.readInteger("Day of the expedition list: ");
+
+        if (!this.handler.setDay(day)) {
+            System.err.println("error: no valid export list for that day");
+            return;
+        }
+
+        // TODO: change variable name
+        var stuff = this.handler.shortestRoute();
+        printPath(stuff.first(), stuff.second());
+        System.out.printf("\nTotal distance: %dm\n", stuff.third());
+
+        var orders = this.handler.ordersByHub();
+        printOrders(orders);
 	}
+
+    private void printPath(List<User> users, List<Distance> dists) {
+        final int size = users.size();
+
+        System.out.println("Path to take:");
+        for (int i = 1; i < size; i++) {
+            System.out.printf("%-5s -> %-5s (%dm)\n", users.get(i-1),
+                              users.get(i), dists.get(i-1));
+        }
+    }
+
+    private void printOrders(Map<User, List<List<Order>>> map) {
+        System.out.println("Bundles delivered in each hub");
+
+        map.forEach((hub, bundles) -> {
+            System.out.printf("Hub: %s", hub);
+            System.out.println("Bundles:");
+
+            for (var bundle : bundles)
+                System.out.printf("\t%s\n", bundle);
+            System.out.println();
+        });
+    }
 }
